434d62cc55066cf5c3fd853fa198db4e071ebad2
TSOlami <tijani.saheed@yahoo.com>
2024-07-08 13:07:46 +0100

    chore: Update Escrow.js with ES6 import syntax

--- test/Escrow.js
+++ test/Escrow.js
@@ -1,5 +1,5 @@
-import { expect } from 'chai';
-import { ethers } from 'hardhat';
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
 const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), 'ether')
@@ -7,7 +7,34 @@ const tokens = (n) => {
 
 describe('Escrow', () => {
 
+    let buyer, seller, lender, inspector
+
     it('Saves the addresses', async () => {
-        ethers.getContractFactory()
+
+        // Get the signers
+        [buyer, seller, lender, inspector] = await ethers.getSigners();
+
+        // Deploy the contract
+        const RealEstate =  await ethers.getContractFactory('RealEstate');
+        const realEstate = await RealEstate.deploy();
+
+        // Mint the property
+        let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS");
+        await transaction.wait();
+
+        const Escrow = await ethers.getContractFactory('Escrow');
+        escrow = await Escrow.deploy(
+            realEstate.address,
+            seller.address,
+            inspector.address,
+            lender.address
+        );
+
+        let result = await escrow.nftAddress();
+
+        expect(result).to.be.equal(realEstate.address);
+
+        result = await escrow.seller();
+        expect(result).to.be.equal(seller.address);
     });
 })
