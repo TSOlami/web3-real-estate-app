12649710cc1b69fe35f1beca678e5001467c3300
TSOlami <tijani.saheed@yahoo.com>
2024-07-10 12:01:54 +0100

    chore: Add listing functionality to Escrow contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -15,7 +15,15 @@ contract Escrow {
     address public lender;
     address payable public seller;
 
+    modifier  onlySeller() {
+        require(msg.sender == seller, "Only the seller can call this function");
+        _;
+    }
+
     mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+    mapping(uint256 => address) public buyer;
 
     constructor(
         address _nftAddress,
@@ -29,11 +37,21 @@ contract Escrow {
         nftAddress = _nftAddress;
     }
 
-    function listNFT(uint256 _tokenId) public {
+    function listNFT(
+        uint256 _tokenId,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount,
+        address _buyer
+    ) public payable onlySeller{
         // Transfer NFT to this contract
         IERC721(nftAddress).transferFrom(msg.sender, address(this), _tokenId);
 
         // Mark NFT as listed
         isListed[_tokenId] = true;
+
+        // Store escrow details
+        purchasePrice[_tokenId] = _purchasePrice;
+        escrowAmount[_tokenId] = _escrowAmount;
+        buyer[_tokenId] = _buyer;
     }
 }
